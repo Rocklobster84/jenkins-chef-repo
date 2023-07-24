77a88ed024d36e4f4840eecd110a1e492b87df91
Stephanie Thompson <thompson@progress.com>
2023-07-24 18:44:11 +0000

    Added default recipe for Apache cookbook

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -3,3 +3,22 @@
 # Recipe:: default
 #
 # Copyright:: 2023, The Authors, All Rights Reserved.
+
+apt_update
+
+package 'net-tools' do
+    action :install
+end
+
+package 'apache2' do
+    action :install
+end
+
+file '/var/www/html/index.html' do
+    action :create
+    content 'Hello World!'
+end
+
+service 'apache2' do
+    action [ :enable, :start ]
+end
